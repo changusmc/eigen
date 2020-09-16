c20763a054cf8b8f1ce1be0940dbcaf2c6981030
David Sheldrick <d.j.sheldrick@gmail.com>
2020-09-16 11:48:35 +0100

    remove partner-locations switchboard logic

--- src/lib/navigation/routes.tsx
+++ src/lib/navigation/routes.tsx
@@ -113,7 +113,7 @@ function getDomainMap(): Record<string, RouteMatcher[] | null> {
     new RouteMatcher("/conditions-of-sale", "WebView"), // remember present_modally
     new RouteMatcher("/artwork-classifications", "ArtworkAttributionClassFAQ"),
 
-    new RouteMatcher("/partner-locations/:id", "PartnerLocations"),
+    new RouteMatcher("/partner-locations/:partnerID", "PartnerLocations"),
 
     new RouteMatcher("/fair/:id/artworks", "FairArtworks"),
     new RouteMatcher("/fair/:id/artists", "FairArtists"),
